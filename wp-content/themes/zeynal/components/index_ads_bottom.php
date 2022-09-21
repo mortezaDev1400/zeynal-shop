fceeb4482064ecfd9c7a1a20e461df01368a2d25
Morteza <test.text@gmail.com>
2022-09-21 08:05:28 +0430

    change file

--- wp-content/themes/zeynal/components/index_ads_bottom.php
+++ wp-content/themes/zeynal/components/index_ads_bottom.php
@@ -1,7 +1,7 @@
 <?php if( get_field('index_ads_bottom','option') ) { ?>
     
     <div class="container mt-5">
-        <div class="index_ads_middle_box" ><?php the_field('index_ads_bottom','option'); ?></div>
+             <div class="index_ads_middle_box" ><?php the_field('index_ads_bottom','option'); ?></div>
     </div>
 
 <?php } ?>
--- wp-content/themes/zeynal/components/index_ads_top.php
+++ wp-content/themes/zeynal/components/index_ads_top.php
@@ -1,7 +1,19 @@
 <?php if( get_field('index_ads_top','option') ) { ?>
     
     <div class="container">
-        <div class="index_ads_middle_box" ><?php the_field('index_ads_top','option'); ?></div>
+<!--       -->
+
+<!--        -->
+        <div class="row">
+<!--            <div class="index_ads_middle_box col-xl-12 col-lg-12 col-md-12 justify-content-center align-items-center" >-->
+
+<!--                <div class=" col-xl-12 col-lg-12 col-md-12 ">-->
+
+                    <?php the_field('index_ads_top','option'); ?>
+<!--                </div>-->
+<!--        </div>-->
+
+            </div>
     </div>
 
 <?php } ?>
